0da568eaf95015328cb048eeafa0dc0d6aa8fc6b
Shawn Chang <yxchang@amazon.com>
2022-06-23 12:43:36 -0700

    Fix BWC for ParquetToSparkSchemaConverter

--- hudi-client/hudi-spark-client/src/main/scala/org/apache/hudi/HoodieSparkUtils.scala
+++ hudi-client/hudi-spark-client/src/main/scala/org/apache/hudi/HoodieSparkUtils.scala
@@ -63,6 +63,8 @@ object HoodieSparkUtils extends SparkAdapterSupport {
 
   def gteqSpark3_2_1: Boolean = SPARK_VERSION >= "3.2.1"
 
+  def isSpark3_3: Boolean = SPARK_VERSION.startsWith("3.3")
+
   def gteqSpark3_3_0: Boolean = SPARK_VERSION >= "3.3.0"
 
   def getMetaSchema: StructType = {
