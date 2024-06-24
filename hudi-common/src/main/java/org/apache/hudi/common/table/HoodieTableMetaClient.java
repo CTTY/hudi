c473c194c792b49b07c58667c41f516370c6c641
Shawn Chang <yxchang@amazon.com>
2024-06-23 21:28:18 -0700

    wip: adding storage strat to hadoop storage, need to revisit some classes

--- hudi-common/src/main/java/org/apache/hudi/common/table/HoodieTableMetaClient.java
+++ hudi-common/src/main/java/org/apache/hudi/common/table/HoodieTableMetaClient.java
@@ -415,6 +415,7 @@ public class HoodieTableMetaClient implements Serializable {
 
   public void setHoodieStorage(HoodieStorage storage) {
     this.storage = storage;
+    if (storage.getStorageStrategy() != null) setStorageStrategy(storageStrategy);
   }
 
   public HoodieStorage getRawHoodieStorage() {
