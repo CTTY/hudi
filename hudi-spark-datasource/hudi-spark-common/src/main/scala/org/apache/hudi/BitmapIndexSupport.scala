ebcaeb1e6be90a3c88a062c15541b0050839e047
Shawn Chang <yxchang@amazon.com>
2025-04-07 15:19:16 -0700

    Clean up bitmapIndexSupport code

--- hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/BitmapIndexSupport.scala
+++ hudi-spark-datasource/hudi-spark-common/src/main/scala/org/apache/hudi/BitmapIndexSupport.scala
@@ -4,14 +4,14 @@ import org.apache.hudi.common.config.HoodieMetadataConfig
 import org.apache.hudi.common.model.FileSlice
 import org.apache.hudi.common.table.HoodieTableMetaClient
 import org.apache.hudi.common.table.log.LogReaderUtils
-import org.apache.hudi.metadata.{BitmapIndexRecordGenerationUtils, HoodieTableMetadataUtil, MetadataPartitionType}
+import org.apache.hudi.metadata.{HoodieTableMetadataUtil, MetadataPartitionType}
 import org.apache.hudi.util.JavaScalaConverters
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.expressions.{AttributeReference, EqualTo, Expression, Literal, Not}
-import org.roaringbitmap.longlong.{Roaring64Bitmap, Roaring64NavigableMap}
+import org.roaringbitmap.longlong.Roaring64NavigableMap
 import org.slf4j.LoggerFactory
 
-import java.util
+import org.apache.hudi.metadata.BitmapIndexRecordGenerationUtils.constructBitmapKey
 import scala.collection.mutable.ArrayBuffer
 
 class BitmapIndexSupport(spark: SparkSession,
@@ -39,7 +39,7 @@ class BitmapIndexSupport(spark: SparkSession,
     val intersectedColumns = queryReferencedColumns.intersect(JavaScalaConverters.convertJavaListToScalaSeq(indexedColumns))
     if (intersectedColumns.isEmpty) {
       log.debug(s"No columns in the query are indexed with bitmap index, skip pruning")
-      // TODO return all file names here
+      return Option(prunedPartitionsAndFileSlices.flatMap(pair => getAllFileNames(pair._2)).toSet)
     }
     // bitmap index should filter on EqualTo NotEqualTo
     // construct keys and use the keys to search for bitmaps optimistically
@@ -58,78 +58,87 @@ class BitmapIndexSupport(spark: SparkSession,
     }
 
     if (equalToArray.isEmpty && notEqualToArray.isEmpty) {
-      // TODO return early
+      log.debug(s"No EqualTo or NotEqualTo query filters to utilize bitmap index, skip pruning")
+      return Option(prunedPartitionsAndFileSlices.flatMap(pair => getAllFileNames(pair._2)).toSet)
     }
 
-    val fileNames = prunedPartitionsAndFileSlices.map(pair => {
-      val partition = pair._1.map(_.path).getOrElse(".")
-      val fileSlices = pair._2
+    val candidateFileNames: Set[String] = prunedPartitionsAndFileSlices.flatMap(pair => {
+      val partition: String = pair._1.map(_.path).getOrElse(".")
+      val fileSlices: Seq[FileSlice] = pair._2
+      // determine if the entire file slice is a candidate
       val fileId = fileSlices.head.getFileId
-      var bitmap : Roaring64NavigableMap = null
-      for (eq: EqualTo <- equalToArray) {
-        val colName = eq.left.asInstanceOf[AttributeReference].name
-        val colVal = eq.right.asInstanceOf[Literal].toString()
-        val bitmapRecordKey = BitmapIndexRecordGenerationUtils
-          .constructBitmapKey(colName, colVal, partition, fileId)
-        val bitmapKeyList = new java.util.ArrayList[String]
-        bitmapKeyList.add(bitmapRecordKey)
-        // TODO get the actual
-        if (bitmap == null) {
-          // TODO remove this test block
-          val test = metadataTable
-            .getRecordsByKeyPrefixes(bitmapKeyList, MetadataPartitionType.BITMAP_INDEX.getPartitionPath, false)
-            .collectAsList()
-          bitmap = metadataTable
-            .getRecordsByKeyPrefixes(bitmapKeyList, MetadataPartitionType.BITMAP_INDEX.getPartitionPath, false)
-            .map(record =>
-              LogReaderUtils.decodeRecordPositionsHeader(record.getData.getBitmapIndexMetadata.get().getBitmap)).collectAsList().get(0)
-        } else {
-          val another = metadataTable
-            .getRecordsByKeyPrefixes(bitmapKeyList, MetadataPartitionType.BITMAP_INDEX.getPartitionPath, false)
-            .map(record =>
-              LogReaderUtils.decodeRecordPositionsHeader(record.getData.getBitmapIndexMetadata.get().getBitmap)).collectAsList().get(0)
-          if (another != null) bitmap.and(another)
-        }
-      }
-
-      for (neq <- notEqualToArray) {
-        val colName = neq.left.asInstanceOf[AttributeReference].name
-        val colVal = neq.right.asInstanceOf[Literal].toString()
-        val bitmapRecordKey = BitmapIndexRecordGenerationUtils
-          .constructBitmapKey(colName, colVal, partition, fileId)
-        val bitmapKeyList = new java.util.ArrayList[String]
-        bitmapKeyList.add(bitmapRecordKey)
-        // TODO maybe add a new method in HoodieMetadataTable interface
-        if (bitmap == null) {
-          bitmap = metadataTable
-            .getRecordsByKeyPrefixes(bitmapKeyList, MetadataPartitionType.BITMAP_INDEX.getPartitionPath, false)
-            .map(record =>
-              LogReaderUtils.decodeRecordPositionsHeader(record.getData.getBitmapIndexMetadata.get().getBitmap)).collectAsList().get(0)
-        } else {
-          val another = metadataTable
-            .getRecordsByKeyPrefixes(bitmapKeyList, MetadataPartitionType.BITMAP_INDEX.getPartitionPath, false)
-            .map(record =>
-              LogReaderUtils.decodeRecordPositionsHeader(record.getData.getBitmapIndexMetadata.get().getBitmap)).collectAsList().get(0)
-          if (another != null) bitmap.andNot(another)
-        }
-      }
-      if (bitmap.getIntCardinality != 0) {
+      var bitmap: Roaring64NavigableMap = null
+      bitmap = checkEqualTo(bitmap, equalToArray, notEqualTo = false, partition, fileId)
+      bitmap = checkEqualTo(bitmap, notEqualToArray, notEqualTo = true, partition, fileId)
+      if (bitmap.getIntCardinality > 0) {
         // is a candidate
-        // TODO fix this logic, need to figure out what is file name exactly
-        fileSlices.head.getBaseFile.get().getFileName
+        // get all filenames in the file slice
+        getAllFileNames(fileSlices)
       } else {
-        // eliminate
+        // eliminate this file slice
         Option.empty
       }
-    }).collect {
-      case fileName: String => fileName
-    }.toSet
+    }).toSet
 
-    Option(fileNames)
+    Option(candidateFileNames)
   }
 
   override def invalidateCaches(): Unit = {
     // do nothing
+  }
+
+  private def checkEqualTo(bitmap: Roaring64NavigableMap,
+                           equalToArray: ArrayBuffer[EqualTo],
+                           notEqualTo: Boolean,
+                           partition: String,
+                           fileId: String): Roaring64NavigableMap = {
+    var varBitmap = bitmap
+    for (eq: EqualTo <- equalToArray) {
+      val colName = eq.left.asInstanceOf[AttributeReference].name
+      val colVal = eq.right.asInstanceOf[Literal].toString()
+      val bitmapRecordKey = constructBitmapKey(colName, colVal, partition, fileId)
+      val bitmapKeyList = new java.util.ArrayList[String]
+      bitmapKeyList.add(bitmapRecordKey)
+      if (varBitmap == null) {
+        // initialize bitmap
+        varBitmap = getBitmapFromMetadataTable(bitmapKeyList)
+      } else {
+        // try joining bitmaps
+        val another = getBitmapFromMetadataTable(bitmapKeyList)
+        if (another != null) {
+          if (notEqualTo) {
+            varBitmap.andNot(another)
+          } else {
+            varBitmap.and(another)
+          }
+        }
+      }
+    }
+    varBitmap
+  }
+
+  private def getBitmapFromMetadataTable(bitmapKeyList: java.util.ArrayList[String]): Roaring64NavigableMap = {
+    // TODO maybe add try-catch here and skip if bitmap is not found
+    metadataTable
+      .getRecordsByKeyPrefixes(bitmapKeyList, MetadataPartitionType.BITMAP_INDEX.getPartitionPath, false)
+      .map(record =>
+        LogReaderUtils.decodeRecordPositionsHeader(record.getData.getBitmapIndexMetadata.get().getBitmap))
+      .collectAsList()
+      .get(0)
+  }
+
+  private def getAllFileNames(fileSlices: Seq[FileSlice]): Seq[String] = {
+    fileSlices.flatMap(fileSlice => {
+      val logFileNames: Array[String] = fileSlice.getLogFiles.map(_.getFileName).toArray[String](filename => new Array[String](filename))
+      val baseFileOpt = fileSlice.getBaseFile
+      val allFileNames: Array[String] = if (baseFileOpt.isPresent) {
+        val baseFileName: String = baseFileOpt.get().getFileName
+        baseFileName +: logFileNames
+      } else {
+        logFileNames
+      }
+      allFileNames
+    })
   }
 
   object BitmapIndexSupport {
